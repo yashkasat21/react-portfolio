9e325e67ddf0123b440d77ee63bcc2897ed55b14
yashkasat21 <yashkasat32@gmail.com>
2023-02-27 17:13:31 +0530

    refactoring code

--- src/index.js
+++ src/index.js
@@ -1,7 +1,19 @@
 import React from 'react';
 import ReactDOM, { createRoot } from 'react-dom/client';
+import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { createBrowserHistory } from 'history';
+import Header from './components/Header'
 import App from './components/App';
+import Jokes from './components/Jokes';
+
 import './index.css';
 
 const root = ReactDOM.createRoot( document.getElementById('root'));
-root.render(<App />);
+root.render(
+    <BrowserRouter history = {createBrowserHistory()}>
+        <Routes>
+            <Route path = '/' element={<Header><App /></Header>} />
+            <Route path = '/jokes' element={<Header><Jokes /></Header>} />
+        </Routes>    
+    </BrowserRouter>
+);
